7aab6a39c0aad2888918b513e5b0db64f8f892b8
Ralph Lepore <ralphlepore@gmail.com>
2015-12-12 16:10:13 -0500

    Added main to setup file.  tinyint fix caused the script to break on my data set so I rolled it back.  Don't need tinyint(4) feature.

--- setup.py
+++ setup.py
@@ -6,4 +6,9 @@ setup(
     url="https://github.com/lanyrd/mysql-postgresql-converter/blob/master/README.md",
     description="Lanyrd's MySQL to PostgreSQL conversion script.",
     author="Lanyrd",
+    entry_points={
+        'console_scripts': [
+            'db_converter = db_converter:main'
+        ]
+    }
 )
